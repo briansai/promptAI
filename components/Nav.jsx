a274311ccf84367291b3e27521c7e9f8195c09ed
Brian Sai <brianwsai@gmail.com>
2023-06-22 19:41:23 -0700

    Created visual links for post and sign out

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,42 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <Link href="/" className="flex gap-2 flex-center as={'image'}">
+      <Link href="/" className="flex gap-2 flex-center" as="image">
         <Image
           src="/assets/images/logo.svg"
-          width="30"
-          height="30"
+          width={30}
+          height={30}
           alt="logo"
         />
         <p className="logo_text">PromptAI</p>
       </Link>
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </nav>
   );
 };
