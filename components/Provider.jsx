3236cc0075ae616d6dd15c4e766aa2864c7bdeef
Brian Sai <brianwsai@gmail.com>
2023-06-24 16:56:06 -0700

    Created Provider and wrapped entire application

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,5 +1,13 @@
-const Provider = () => {
-  return <div>Provider</div>;
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+const Provider = ({ children, session }) => {
+  return (
+    <div>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </div>
+  );
 };
 
 export default Provider;
