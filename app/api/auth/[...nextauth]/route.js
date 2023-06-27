4bcb2dd0a67614c3fa30beb775b0305131e5b5fd
Brian Sai <brianwsai@gmail.com>
2023-06-26 19:15:34 -0700

    Included user session in auth handler

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,15 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.mail,
+    });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDB();
@@ -27,7 +35,7 @@ const handler = NextAuth({
       }
       return true;
     } catch (err) {
-      console.log(error);
+      console.log(err);
       return false;
     }
   },
