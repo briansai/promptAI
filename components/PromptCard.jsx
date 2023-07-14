fd7a7340e408d4f1c5ee26eec9b44d2fbf6ebb43
Brian Sai <brianwsai@gmail.com>
2023-07-13 18:08:49 -0700

    Implemented text copy function and onclick functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,13 @@ import { UseSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState('');
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(''), 3000);
+  };
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -25,8 +32,27 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
               {post.creator.email}
             </p>
           </div>
+          <div className="copy_btn" onClick={handleCopy}>
+            <Image
+              src={
+                copied === post.prompt
+                  ? '/assets/icons/tick.svg'
+                  : '/assets/icons/copy.svg'
+              }
+              alt="copy_button"
+              width={12}
+              height={12}
+            />
+          </div>
         </div>
       </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="'font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   );
 };
